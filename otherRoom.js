cf9ae0be17ce1dc7ce6d57b4d53df91e331b2bb8
Annie <wanghuiting.dalian@gmail.com>
2015-11-09 11:28:44 +0100

    skybox trying

--- otherRoom.js
+++ otherRoom.js
@@ -2,25 +2,45 @@ var scene, camera, renderer;
     var geometry, material, mesh;
 
     init();
-    animate();
+    //animate();
 
     function init() {
 
         scene = new THREE.Scene();
 
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera.position.z = 1000;
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 100000 );
+        //camera.position.z = 1000;
 
-        geometry = new THREE.BoxGeometry( 200, 200, 200 );
-        material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+        // load the cube textures
+        var urlPrefix   = "Images/";
+        var urls = [ urlPrefix + "posx.jpg", urlPrefix + "negx.jpg",
+            urlPrefix + "posy.jpg", urlPrefix + "negy.jpg",
+            urlPrefix + "posz.jpg", urlPrefix + "negz.jpg" ];
+        var textureCube = THREE.ImageUtils.loadTextureCube( urls );
 
-        mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
+        // init the cube shadder
+        var shader  = THREE.ShaderLib["cube"];
+        var uniforms    = THREE.UniformsUtils.clone( shader.uniforms );
+        uniforms['tCube'].texture= textureCube;
+        var material = new THREE.ShaderMaterial({
+            fragmentShader  : shader.fragmentShader,
+            vertexShader    : shader.vertexShader,
+            uniforms    : uniforms
+        });
+
+        //geometry = new THREE.BoxGeometry( 400, 400, 400 );
+        geometry = new THREE.CubeGeometry( 100000, 100000, 100000, 1, 1, 1, null, true )
+        //material = new THREE.MeshBasicMaterial( { color: 0xff0000} );
+
+        skyboxMesh  = new THREE.Mesh( geometry, material );
+        scene.add( skyboxMesh );
 
         renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.render( scene, camera );
 
         document.body.appendChild( renderer.domElement );
+
 
     }
 
